f936c9d7f794978ca3b2b334d9fc7644d2db3c36
José Céspedes <jose.alvaradocespedes@ucr.ac.cr>
2021-05-29 04:37:17 -0600

    Finished 21-005

--- BackEnd_Java_Server/src/TFTPserverRRQ.java
+++ BackEnd_Java_Server/src/TFTPserverRRQ.java
@@ -18,7 +18,7 @@ class TFTPserverRRQ extends Thread {
 	protected String fileName;
 	protected String folderName;
 	private String path = "C:\\21-IF5000-TFTP\\BackEnd_Java_Server\\images\\";
-	protected int[] ramdonBlkNum;
+
 
 	// initialize read request
 	public TFTPserverRRQ(TFTPread request) throws TftpException {
@@ -66,10 +66,31 @@ class TFTPserverRRQ extends Thread {
 			System.out.println("Client start failed:  " + e.getMessage());
 		}
 	}
-	//https://es.stackoverflow.com/questions/38599/crear-n%C3%BAmeros-aleatorios-sin-que-se-repitan
-	public int[] ramdonArray(int size){
-		int[] numerosAleatorios = IntStream.rangeClosed(1, size).toArray();
 
+	//https://es.stackoverflow.com/questions/38599/crear-n%C3%BAmeros-aleatorios-sin-que-se-repitan
+//	public int[] ramdonArray(int size){
+//		int[] numerosAleatorios = new int[size];
+//		for (int i = 1; i <= 20; i++) {
+//			numerosAleatorios[i] = i;
+//		}
+//		Random r = new Random();
+//		for (int i = numerosAleatorios.length; i > 0; i--) {
+//			int posicion = r.nextInt(i);
+//			int tmp = numerosAleatorios[i-1];
+//			numerosAleatorios[i - 1] = numerosAleatorios[posicion];
+//			numerosAleatorios[posicion] = tmp;
+//		}
+//		return  numerosAleatorios;
+//	}
+
+	//everything is fine, open new thread to transfer file
+	public void run() {
+		int bytesRead = TFTPpacket.maxTftpPakLen;
+
+		int[] numerosAleatorios = new int[10000];
+		for (int i = 0; i < numerosAleatorios.length; i++) {
+			numerosAleatorios[i] = i;
+		}
 		Random r = new Random();
 		for (int i = numerosAleatorios.length; i > 0; i--) {
 			int posicion = r.nextInt(i);
@@ -77,38 +98,31 @@ class TFTPserverRRQ extends Thread {
 			numerosAleatorios[i - 1] = numerosAleatorios[posicion];
 			numerosAleatorios[posicion] = tmp;
 		}
-		return  numerosAleatorios;
-	}
 
-	//everything is fine, open new thread to transfer file
-	public void run() {
-		int bytesRead = TFTPpacket.maxTftpPakLen;
-		ramdonBlkNum=  ramdonArray(TFTPpacket.maxTftpPakLen);
-		int ramdonBlk = 0;
 		// handle read request
 		if (req instanceof TFTPread) {
 			try {
 				for (int blkNum = 1; bytesRead == TFTPpacket.maxTftpPakLen; blkNum++) {
-					ramdonBlk = ramdonBlkNum[blkNum-1];
-
-					TFTPdata outPak = new TFTPdata(ramdonBlk, source);
+					TFTPdata outPak = new TFTPdata(numerosAleatorios[blkNum-1], source);
 					bytesRead = outPak.getLength();
 					outPak.send(host, port, sock);
 
+
+
 					//wait for the correct ack. if incorrect, retry up to 5 times
-					while (timeoutLimit!=0) { 
+					while (timeoutLimit!=0) {
 						try {
 							TFTPpacket ack = TFTPpacket.receive(sock);
 							if (!(ack instanceof TFTPack)){throw new Exception("Client failed");}
 							TFTPack a = (TFTPack) ack;
-							
-							if(a.blockNumber()!=ramdonBlk){ //check ack
+
+							if(a.blockNumber()!=numerosAleatorios[blkNum-1]){ //check ack
 								throw new SocketTimeoutException("last packet lost, resend packet");}
 							/*System.out.println("confirm blk num " + a.blockNumber()+" from "+a.getPort());*/
 							break;
-						} 
+						}
 						catch (SocketTimeoutException t) {//resend last packet
-							System.out.println("Resent blk " + ramdonBlk);
+							System.out.println("Resent blk " + numerosAleatorios[blkNum-1]);
 							timeoutLimit--;
 							outPak.send(host, port, sock);
 						}
